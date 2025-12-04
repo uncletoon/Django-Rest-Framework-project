c879f4c014982e7096b8ce6d2f10cdaf60c373b0
uncletoon <ingabirepatience3@gmail.com>
2025-12-04 18:25:17 +0200

    Deploy settings

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
+import dj_database_url
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x86g5*+rl-=akyw4hpbzj9atoosesky^5i#s#!d+0a_c_p4f8i'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ") # type: ignore
 
 
 # Application definition
@@ -93,6 +96,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+dataabase_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(dataabase_url) # type: ignore
 
 
 # Password validation
