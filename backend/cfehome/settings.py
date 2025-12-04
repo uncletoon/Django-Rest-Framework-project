7ac7b6b78ae8053aec8dda409ba4de0e0c98e4af
uncletoon <ingabirepatience3@gmail.com>
2025-12-05 00:30:39 +0200

    Fixing database conf

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -23,13 +23,8 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1 localhost").split(" ")
@@ -94,17 +89,20 @@ WSGI_APPLICATION = 'cfehome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 database_url = os.environ.get("DATABASE_URL")
 
 if database_url:
-    DATABASES["default"] = dj_database_url.parse(database_url) # type: ignore
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    DATABASES["default"] = dj_database_url.parse(database_url, conn_max_age=600) # type: ignore
+
+
 
 
 
@@ -195,9 +193,4 @@ SPECTACULAR_SETTINGS = {
     'SWAGGER_UI_DIST': 'SIDECAR',  # shorthand to use the sidecar instead
     'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
     'REDOC_DIST': 'SIDECAR',
-}
-
-SPECTACULAR_SETTINGS = {
-   
-    # OTHER SETTINGS
 }
